cb2d258adf83355022bb37aaba98fc304ca72ae9
Roushan799 <roushan.kumar@arnxt.com>
2024-07-15 17:58:41 +0530

    update

--- src/Luxeadmin.js
+++ src/Luxeadmin.js
@@ -51,11 +51,18 @@ const Luxeadmin = () => {
     ratings: '',
     discription: '',
     tags: '',
-    imagePath: ''
+    imagePath: '',
+    colors: '',
+    room: '',
+    stock: '',
+    material: ''
   })
 
   const [imageData, setImageData] = useState("");
   const [tagArr, setTagArr] = useState([]);
+
+  const [colorArr, setColorArr] = useState([]);
+
   const [paths, setImagePaths] = useState([]);
 
   const [uploading, setUploading] = useState(false);
@@ -78,16 +85,18 @@ const Luxeadmin = () => {
   };
 
   useEffect(() => {
-    setFormData({ ...formData, imagePath: paths, tags: tagArr });
-  }, [paths, tagArr])
+    setFormData({ ...formData, imagePath: paths, tags: tagArr, colors: colorArr });
+  }, [paths, tagArr, colorArr])
+
+
 
   async function storeData() {
     try {
       // console.log(formData)
       // setFormData({ ...formData, imagePath: paths, tags:tagArr});
-      console.log(paths);
+      console.log(formData);
       const data = ({ ...formData, servicesData: serviceList });
-      if (formData.productName === '' || formData.categoryName === '' || formData.style === '' || formData.design === '' || formData.collection === '' || formData.discount === '' || formData.ratings === '' || formData.discription === '' || formData.imagePath === '' || formData.tags.length <= 0) {
+      if (formData.productName === '' || formData.categoryName === '' || formData.style === '' || formData.design === '' || formData.collection === '' || formData.discount === '' || formData.ratings === '' || formData.discription === '' || formData.imagePath === '' || formData.tags.length <= 0 || formData.colors.length <= 0 || formData.room === '' || formData.material === '' || formData.stock === '') {
         return alert("Fill all the fields");
       }
       console.log(data);
@@ -180,6 +189,10 @@ const Luxeadmin = () => {
     setTagArr(prevFiles => prevFiles.filter((_, i) => i !== index));
   };
 
+  const removeColor = (index) => {
+    setColorArr(prevFiles => prevFiles.filter((_, i) => i !== index));
+  };
+
 
   const removeImage = (index) => {
     setGlbFile(prevFiles => prevFiles.filter((e, i) => i !== index));
@@ -264,6 +277,19 @@ const Luxeadmin = () => {
     }
   };
 
+  const handleColorChange = (e) => {
+    const { value } = e.target;
+    if (value !== '') {
+      setColorArr(prevArr => [...prevArr, value]);
+      // Clear the input value after adding the tag
+      e.target.value = '';
+    }
+    if (colorArr.includes(value)) {
+      setColorArr(prevFiles => prevFiles.filter((e) => e !== value));
+    }
+    console.log(e)
+  };
+
   console.log(serviceList)
 
 
@@ -280,20 +306,33 @@ const Luxeadmin = () => {
 
           <div className=''>
             <Select id="categoryName" value={formData.categoryName} className='allTheField' name="categoryName" style={{ width: '223px', margin: 'auto', color: formData.categoryName === '' ? '#c4c4c4' : 'inherit' }} onChange={handleFormChange} displayEmpty inputProps={{ 'aria-label': 'Without label' }}>
-              <MenuItem value="" style={{display:"none"}}>Select Category</MenuItem>
-              <MenuItem value="NEW ARRIVAL">NEW ARRIVAL</MenuItem>
-              <MenuItem value="VIEW ALL RUGS">VIEW ALL RUGS</MenuItem>
-              <MenuItem value="BESTSELLERS">BEST SELLERS</MenuItem>
-              <MenuItem value="RUG SWATCH">RUG SWATCH</MenuItem>
-              <MenuItem value="ANTIQUE RUGS">ANTIQUE RUGS</MenuItem>
-              <MenuItem value="SPECIAL PRICES">SPECIAL PRICES</MenuItem>
-              <MenuItem value="MANCHAHA">MANCHAHA</MenuItem>
+              <MenuItem value="" style={{ display: "none" }}>Select Category</MenuItem>
+              <MenuItem value="Oushak">Oushak</MenuItem>
+              <MenuItem value="Mid Century Modern">Mid Century Modern</MenuItem>
+              <MenuItem value="Persian">Persian</MenuItem>
+              <MenuItem value="Hype Master">Hype Master</MenuItem>
+              <MenuItem value="Trending, Loved & Liked">Trending, Loved & Liked</MenuItem>
+              <MenuItem value="Geometrical">Geometrical</MenuItem>
+              <MenuItem value="Abstract">Abstract</MenuItem>
+              <MenuItem value="Solid">Solid</MenuItem>
+              <MenuItem value="Flatweave Modern">Flatweave Modern</MenuItem>
+              <MenuItem value="Flatweave Traditional">Flatweave Traditional</MenuItem>
+              <MenuItem value="Narurals">Narurals</MenuItem>
+              <MenuItem value="Kids">Kids</MenuItem>
+              <MenuItem value="Shaggy">Shaggy</MenuItem>
+              <MenuItem value="Braids & Pebbles">Braids & Pebbles</MenuItem>
+              <MenuItem value="Contemporary">Contemporary</MenuItem>
+              <MenuItem value="Minimalist - Hand Carved">Minimalist - Hand Carved</MenuItem>
+              <MenuItem value="Minimalist">Minimalist</MenuItem>
+              <MenuItem value="Shaped">Shaped</MenuItem>
+              <MenuItem value="Floral">Floral</MenuItem>
+              <MenuItem value="Ultra Luxury">Ultra Luxury</MenuItem>
             </Select>
           </div>
 
           <div className=''>
             <Select id="styleName" value={formData.style} name="style" style={{ width: '223px', margin: 'auto', color: formData.style === '' ? '#c4c4c4' : 'inherit' }} onChange={handleFormChange} displayEmpty inputProps={{ 'aria-label': 'Without label' }}>
-              <MenuItem value="" style={{display:"none"}}>Select Style</MenuItem>
+              <MenuItem value="" style={{ display: "none" }}>Select Style</MenuItem>
               <MenuItem value="Modern">Modern</MenuItem>
               <MenuItem value="Transitional">Transitional</MenuItem>
               <MenuItem value="Traditional">Traditional</MenuItem>
@@ -301,7 +340,7 @@ const Luxeadmin = () => {
           </div>
           <div className=''>
             <Select id="designName" value={formData.design} name="design" style={{ width: '223px', color: formData.design === '' ? '#c4c4c4' : 'inherit' }} onChange={handleFormChange} displayEmpty inputProps={{ 'aria-label': 'Without label' }}>
-              <MenuItem value="" style={{display:"none"}}>Select Design</MenuItem>
+              <MenuItem value="" style={{ display: "none" }}>Select Design</MenuItem>
               <MenuItem value="Abstract">Abstract</MenuItem>
               <MenuItem value="Oriental and Traditional">Oriental and Traditional</MenuItem>
               <MenuItem value="Moroccan and Tribal">Moroccan and Tribal</MenuItem>
@@ -315,7 +354,7 @@ const Luxeadmin = () => {
           </div>
           <div className=''>
             <Select id="collectionName" value={formData.collection} name="collection" style={{ width: '223px', color: formData.collection === '' ? '#c4c4c4' : 'inherit' }} onChange={handleFormChange} displayEmpty inputProps={{ 'aria-label': 'Without label' }}>
-              <MenuItem value="" style={{display:"none"}}>Select Collection</MenuItem>
+              <MenuItem value="" style={{ display: "none" }}>Select Collection</MenuItem>
               <MenuItem value="Acar">Acar</MenuItem>
               <MenuItem value="Aurora">Aurora</MenuItem>
               <MenuItem value="Clan">Clan</MenuItem>
@@ -328,12 +367,85 @@ const Luxeadmin = () => {
           </div>
           <div className=''>
             <TextField id="outlined-basic" label="Discount" variant="outlined" name='discount' value={formData.discount} onChange={handleFormChange} required InputLabelProps={{ sx: { '& .MuiFormLabel-asterisk': { color: 'red', fontSize: '1.5rem' } } }} />
-
           </div>
-          <div className=''>
-            <TextField id="outlined-basic" type='number' label="Ratings" variant="outlined" name='ratings' value={formData.ratings} onChange={handleFormChange} required InputLabelProps={{ sx: { '& .MuiFormLabel-asterisk': { color: 'red', fontSize: '1.5rem' } } }} />
 
+          {/* color start */}
+
+          <div>
+            <div
+              style={{
+                height: '200px',
+                width: '223px',
+                margin: 'auto',
+                padding: '7px',
+                borderRadius: '5px',
+                border: '1px solid #c4c4c4',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                overflowY: 'auto',
+                overflowX: 'auto',
+              }}
+              variant="outlined"
+            >
+              <div
+                style={{
+                  display: 'grid',
+                  gridTemplateColumns: 'repeat(2, 1fr)',
+                  gap: '3px',
+                  marginBottom: '10px',
+                }}
+              >
+                {colorArr.map((e, i) => (
+                  <div
+                    key={i}
+                    style={{
+                      border: '1px solid black',
+                      borderRadius: '7px',
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                      padding: '3px',
+                    }}
+                  >
+                    {e}
+                    <CancelIcon
+                      style={{ height: '2.5vh', width: '2.5vw', cursor: 'pointer' }}
+                      onClick={() => removeColor(i)}
+                    />
+                  </div>
+                ))}
+              </div>
+              <Select
+                id="styleTags"
+                value={''}
+                name="color"
+                style={{
+                  width: '207px',
+                  color: formData.colors.length === 0 ? '#c4c4c4' : 'inherit',
+                }}
+                onChange={handleColorChange}
+                displayEmpty
+                inputProps={{ 'aria-label': 'Without label' }}
+              >
+                <MenuItem style={{ display: 'none' }} value="">
+                  Select Colors
+                </MenuItem>
+                {['Green', 'Blue', 'Orange', 'Red', 'Yellow'].map((color) => (
+                  <MenuItem key={color} value={color}>
+                    {colorArr.includes(color) && <CheckIcon style={{ color: 'green' }} />}
+                    {color}
+                  </MenuItem>
+                ))}
+              </Select>
+            </div>
           </div>
+
+
+          {/* color end */}
+
+
+
           <div className=''>
             <div style={{ height: '200px', width: '223px', margin: 'auto', padding: '7px', borderRadius: '5px', border: '1px solid #c4c4c4', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', overflowY: 'auto', overflowX: 'auto', }} variant="outlined">
               <div style={{ display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gap: '3px', marginBottom: '10px' }}>
@@ -345,8 +457,11 @@ const Luxeadmin = () => {
                 })}
               </div>
               <Select id="styleTags" value={''} name="tags" style={{ width: '207px', color: formData.tags.length === 0 ? '#c4c4c4' : 'inherit' }} onChange={handletagchange} displayEmpty inputProps={{ 'aria-label': 'Without label' }}>
-                <MenuItem style={{display:"none"}} value="">Select Tags</MenuItem>
-                <MenuItem value="Letest"> {tagArr.includes("Letest") ? <CheckIcon style={{ color: 'green' }} /> : null} Letest</MenuItem>
+                <MenuItem style={{ display: "none" }} value="">Select Tags</MenuItem>
+                <MenuItem value="Top Selling"> {tagArr.includes("Top Selling") ? <CheckIcon style={{ color: 'green' }} /> : null} Top Selling</MenuItem>
+                <MenuItem value="Trending Products"> {tagArr.includes("Trending Products") ? <CheckIcon style={{ color: 'green' }} /> : null} Trending Products</MenuItem>
+                <MenuItem value="Recently Added"> {tagArr.includes("Recently Added") ? <CheckIcon style={{ color: 'green' }} /> : null} Recently Added</MenuItem>
+
                 <MenuItem value="Modern"> {tagArr.includes("Modern") ? <CheckIcon style={{ color: 'green' }} /> : null} Modern</MenuItem>
                 <MenuItem value="Transitional">{tagArr.includes("Transitional") ? <CheckIcon style={{ color: 'green' }} /> : null} Transitional</MenuItem>
                 <MenuItem value="Traditional">{tagArr.includes("Traditional") ? <CheckIcon style={{ color: 'green' }} /> : null} Traditional</MenuItem>
@@ -357,12 +472,55 @@ const Luxeadmin = () => {
                 <MenuItem value="Aurora"> {tagArr.includes("Aurora") ? <CheckIcon style={{ color: 'green' }} /> : null} Aurora</MenuItem>
                 <MenuItem value="Cyanna"> {tagArr.includes("Cyanna") ? <CheckIcon style={{ color: 'green' }} /> : null} Cyanna</MenuItem>
               </Select>
-            </div>  
+            </div>
           </div>
 
           <div className=''>
             <textarea id="AreaDescription" label="Description" variant="outlined" placeholder='Description *' name='discription' value={formData.discription} onChange={handleFormChange} required InputLabelProps={{ sx: { '& .MuiFormLabel-asterisk': { color: 'red', fontSize: '1.5rem' } } }} />
           </div>
+
+
+          {/* rating, room, material, stock start */}
+
+
+          <div className=''>
+            <TextField id="outlined-basic" type='number' label="Ratings" variant="outlined" name='ratings' value={formData.ratings} onChange={handleFormChange} required InputLabelProps={{ sx: { '& .MuiFormLabel-asterisk': { color: 'red', fontSize: '1.5rem' } } }} />
+          </div>
+
+
+          <div className=''>
+            <Select id="roomName" value={formData.room} name="room" style={{ width: '223px', color: formData.room === '' ? '#c4c4c4' : 'inherit' }} onChange={handleFormChange} displayEmpty inputProps={{ 'aria-label': 'Without label' }}>
+              <MenuItem value="" style={{ display: "none" }}>Select Room</MenuItem>
+              <MenuItem value="Living Room">Living Room</MenuItem>
+              <MenuItem value="Dining Room">Dining Room</MenuItem>
+              <MenuItem value="Bedroom">Bedroom</MenuItem>
+              <MenuItem value="Kids Room">Kids Room</MenuItem>
+            </Select>
+          </div>
+
+
+
+          <div className=''>
+            <Select id="materialName" value={formData.material} name="material" style={{ width: '223px', color: formData.material === '' ? '#c4c4c4' : 'inherit' }} onChange={handleFormChange} displayEmpty inputProps={{ 'aria-label': 'Without label' }}>
+              <MenuItem value="" style={{ display: "none" }}>Select Material</MenuItem>
+              <MenuItem value="Wool">Wool</MenuItem>
+              <MenuItem value="Wool & Bamboo Silk">Wool & Bamboo Silk</MenuItem>
+              <MenuItem value="Wool & Silk">Wool & Silk</MenuItem>
+              <MenuItem value="Silk">Silk</MenuItem>
+              <MenuItem value="Viscose">Viscose</MenuItem>
+              <MenuItem value="Jute & Hemp">Jute & Hemp</MenuItem>
+              <MenuItem value="Cotton">Cotton</MenuItem>
+              <MenuItem value="Others">Others</MenuItem>
+            </Select>
+          </div>
+
+
+          <div className=''>
+            <TextField id="outlined-basic" type='number' label="Stock" variant="outlined" name='stock' value={formData.stock} onChange={handleFormChange} required InputLabelProps={{ sx: { '& .MuiFormLabel-asterisk': { color: 'red', fontSize: '1.5rem' } } }} />
+          </div>
+
+
+          {/*  */}
 
           <div>
             <div>
@@ -372,7 +530,7 @@ const Luxeadmin = () => {
                   serviceList && serviceList.map((item, index) => (
                     <div key={index} className='' style={{ display: 'grid', gridTemplateColumns: "repeat(4, 1fr)", gap: '10px', marginLeft: '10px', justifyContent: 'center', alignItems: 'center', marginTop: '15px' }} >
                       <Select id="styleSize" value={item.productsize ? item.productsize : ""} name="productsize" onChange={(e) => handleServiceChange(e, index)} style={{ color: item.productsize === '' ? '#c4c4c4' : 'inherit' }} displayEmpty inputProps={{ 'aria-label': 'Without label' }}>
-                        <MenuItem style={{display:"none"}} value="">Select Size</MenuItem>
+                        <MenuItem style={{ display: "none" }} value="">Select Size</MenuItem>
                         <MenuItem value="60X90">60X90</MenuItem>
                         <MenuItem value="90X150">90X150</MenuItem>
                         <MenuItem value="120X180">120X180</MenuItem>
@@ -436,9 +594,9 @@ const Luxeadmin = () => {
                         variant='outlined'
                         disabled
                         value={item.productgst ? item.productgst : "GST"}
-                        // onChange={(e) => handleServiceChange(e, index)}
-                        // required
-                        // InputLabelProps={{ sx: { '& .MuiFormLabel-asterisk': { color: 'red', fontSize: '1.5rem' } } }}
+                      // onChange={(e) => handleServiceChange(e, index)}
+                      // required
+                      // InputLabelProps={{ sx: { '& .MuiFormLabel-asterisk': { color: 'red', fontSize: '1.5rem' } } }}
                       />
 
 
