4d7d7c798694d10518cee6600b94bec1fbbb49ef
anil-arnxt <anil.samal@arnxt.com>
2024-07-19 10:37:31 +0530

    new changes

--- src/GenerateQrcode.js
+++ src/GenerateQrcode.js
@@ -41,7 +41,7 @@ const GenerateQrcode = () => {
               </div>
               <div className='brandqrcodecontainer'>
                 <div className='qrcodeinsidediv' >
-                {/* <QRCode  size={400} style={{padding:'10px'}} viewBox='0 0 200 200' value={`https://arhorizon.in/arvideo/index.html?id=1713162542905`} /> */}
+                <QRCode  size={200} style={{padding:'10px'}} viewBox='0 0 200 200' value={`https://play.google.com/store/apps/details?id=com.quleep.ARnxt&pcampaignid=web_share`} />
                     {
                         brandqrcodedata && brandqrcodedata.map((item,i)=>(
                             <div>
